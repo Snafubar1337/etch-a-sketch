f0f807c7edea34f5ade8faf018994100124b9eb0
Snafubar1337 <snafu.alit@gmail.com>
2022-06-21 21:27:21 +0200

    ADD functionality

--- javascript.js
+++ javascript.js
@@ -1,15 +1,23 @@
 const mainContainer = document.querySelector("#mainContainer");
+const refreshButton = document.querySelector("button");
 
 function createSquare(num = 16){
     for (divI = 1; divI <= num;divI++){
         for(divJ = 1; divJ<= num;divJ++){
             const div = document.createElement('div');
-            let tempInt = (divI-1)*16 + divJ;
+            let tempInt = (divI-1)*num + divJ;
             let tempStr = String(tempInt);
             div.setAttribute("id", tempStr);
+            div.style.width = String(800/num)+"px";
+            div.style.height = String(800/num)+"px";
             div.classList.add("box");
             mainContainer.appendChild(div);
         }
+    }
+}
+function removeSquare(){
+    while(mainContainer.hasChildNodes()){
+        mainContainer.removeChild(mainContainer.firstChild);
     }
 }
 function fill(){
@@ -20,8 +28,18 @@ function fill(){
      })
     })
 }
-createSquare();
+refreshButton.addEventListener("click", ()=>{
+    let temp = prompt("How many squares per side do you want?");
+    if (temp <= 100 && temp >0){
+        removeSquare();
+        createSquare(temp);
+        fill();
+    }
+    else alert("Invalid, pal!");
+})
 
+createSquare();
+fill();
 
 /*
 1. create 16x16 div using JS
